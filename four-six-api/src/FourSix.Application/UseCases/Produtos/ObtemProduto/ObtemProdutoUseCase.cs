1e647fce4964886746e2ff76e9f3d45f91c430f6
Jair Schwan <jair.schwan9@gmail.com>
2023-10-22 20:05:21 -0300

    add new endpoint to get product by id

---
+++ four-six-api/src/FourSix.Application/UseCases/Produtos/ObtemProduto/ObtemProdutoUseCase.cs
@@ -0,0 +1,35 @@
+﻿using FourSix.Domain.Entities.ProdutoAggregate;
+
+namespace FourSix.Application.UseCases.Produtos.ObtemProdutos
+{
+    public class ObtemProdutosUseCase : IObtemProdutosUseCase
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private IOutputPort _outputPort;
+
+        public ObtemProdutosUseCase(IProdutoRepository produtoRepository)
+        {
+            this._produtoRepository = produtoRepository;
+            this._outputPort = new ObtemProdutosPresenter();
+        }
+
+        public void SetOutputPort(IOutputPort outputPort) => this._outputPort = outputPort;
+
+        public Task Execute() =>
+            this.ObtemProdutos();
+
+        private async Task ObtemProdutos()
+        {
+            var produtos = this._produtoRepository
+                .Listar().ToList();
+
+            if (produtos != null)
+            {
+                this._outputPort.Ok(produtos);
+                return;
+            }
+
+            this._outputPort.NotFound();
+        }
+    }
+}
